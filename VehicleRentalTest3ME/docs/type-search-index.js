28d3c3180f19303835e035aeb4436916dd4fc69b
Million Eyassu <58177555+e-million@users.noreply.github.com>
2024-02-19 14:31:53 -0800

    Delete VehicleRentalTest3ME/docs directory

--- VehicleRentalTest3ME/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CCar"},{"p":"<Unnamed>","l":"CMotorbike"},{"p":"<Unnamed>","l":"CTrailer"},{"p":"<Unnamed>","l":"CVehicle"},{"p":"<Unnamed>","l":"CVehicleFinal"}]
